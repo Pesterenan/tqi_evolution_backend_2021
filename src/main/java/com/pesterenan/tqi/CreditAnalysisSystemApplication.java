4b2d573fc4b0d884fb2ff15ef5268b477d335454
Renan Torres <pesterenan@gmail.com>
2022-01-03 20:39:38 -0300

    Initiliazed SpringBoot Project

---
+++ src/main/java/com/pesterenan/tqi/CreditAnalysisSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pesterenan.tqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAnalysisSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditAnalysisSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pesterenan/tqi/CreditAnalysisSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesterenan.tqi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAnalysisSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
